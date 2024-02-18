b6920467e8518e78a1506d43866296fc97e0d3b7
devGWeloper <53815271+devGWeloper@users.noreply.github.com>
2024-02-18 13:47:29 +0900

    Add files via upload
    backend Initailize

---
+++ backend/memoryapp/src/main/java/com/subproject/memoryapp/MemoryappApplication.java
@@ -0,0 +1,13 @@
+package com.subproject.memoryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoryappApplication.class, args);
+	}
+
+}
---
+++ backend/memoryapp/src/test/java/com/subproject/memoryapp/MemoryappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subproject.memoryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoryappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
